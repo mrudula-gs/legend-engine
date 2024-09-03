1608513a08dcea49813dd7fec1b9eade96f5b56f
tamimi <mimi.x.tam@ny.email.gs.com>
2024-09-03 09:36:33 -0400

    saving version

--- legend-engine-xts-deephaven/legend-engine-xt-deephaven-grammar/src/main/java/org.finos.legend.engine.language.stores.deephaven/from/DeephavenStoreParseTreeWalker.java
+++ legend-engine-xts-deephaven/legend-engine-xt-deephaven-grammar/src/main/java/org.finos.legend.engine.language.stores.deephaven/from/DeephavenStoreParseTreeWalker.java
@@ -27,7 +27,7 @@ public class DeephavenStoreParseTreeWalker
         this.extension = extension;
     }
 
-    // TODO: ask beyraf - why does DefinitionContext have imports? generated from Pure code but what is purpose and where is that code generated?
+    // TODO: ask beyraf - why does DefinitionContext have imports? This is Generated from Antlr
     // e.g. what generates this code? "public static class ComplexPropertyTypesContext extends ParserRuleContext "?
     public Section visit(String sectionType, Consumer<PackageableElement> elementConsumer, DeephavenParserGrammar.DefinitionContext definitionContext)
     {
@@ -45,6 +45,39 @@ public class DeephavenStoreParseTreeWalker
         this.visit(definitionContext.deephavenStoreDefinition(), sectionConsumer);
 
         return section;
+    }
 
+    private void visit(List<DeephavenParserGrammar.deephavenStoreDefinitionContext> deephavenStoreDefinition, Consumer<PackageableElement> elementConsumer)
+    {
+        deephavenStoreDefinition.stream().map(this::visitDeephavenStore).forEach(elementConsumer);
+    }
+
+    private DeephavenStore visitDeephavenStore(DeephavenParserGrammar.deephavenStoreDefinitionContext ctx)
+    {
+        DeephavenStore store = new DeephavenStore();
+        // required fields for all stores
+        store.name = PureGrammarParserUtility.fromIdentifier(ctx.qualifiedName().identifier());
+        // TODO: ask beyraf or renu - double check why ElasticSearch wouldn't check for packagePath == null but mongo and relational do check that; when would it be null?
+        store._package = ctx.qualifiedName().packagePath() == null ? "" : PureGrammarParserUtility.fromPath(ctx.qualifiedName().packagePath().identifier());
+        store.sourceInformation = this.parserInfo.walkerSourceInformation.getSourceInformation(ctx);
+
+        // fields defined for deephaven
+        store.tables = PureGrammarParserUtility.validateAndExtractRequiredField(ctx.tables(), )
+
+    }
+
+    private Table visitTable(DeephavenParserGrammar.TableContext ctx)
+    {
+        Table table = new Table();
+        table.sourceInformation = this.walkerSourceInformation.getSourceInformation(ctx);
+        table.name = ctx.relationalIdentifier().QUOTED_STRING() == null ? ctx.relationalIdentifier().unquotedIdentifier().getText() : ctx.relationalIdentifier().QUOTED_STRING().getText();
+        List<String> primaryKeys = new ArrayList<>();
+        table.columns = ListIterate.collect(ctx.columnDefinition(), columnDefinitionContext -> this.visitColumnDefinition(columnDefinitionContext, primaryKeys));
+        table.primaryKey = primaryKeys;
+        if (ctx.milestoneSpec() != null)
+        {
+            table.milestoning = ListIterate.collect(ctx.milestoneSpec().milestoning(), this::visitMilestoning);
+        }
+        return table;
     }
 }
