a54af19aed1204b972ba2d6c5d8099894b7da15d
tamimi <mimi.x.tam@ny.email.gs.com>
2024-09-27 14:39:50 -0400

    fixed type assignment for compiler

--- legend-engine-xts-deephaven/legend-engine-xt-deephaven-grammar/src/main/java/org/finos/legend/engine/language/deephaven/compiler/HelperDeephavenStoreBuilder.java
+++ legend-engine-xts-deephaven/legend-engine-xt-deephaven-grammar/src/main/java/org/finos/legend/engine/language/deephaven/compiler/HelperDeephavenStoreBuilder.java
@@ -25,6 +25,18 @@ import org.finos.legend.engine.language.pure.compiler.toPureGraph.SourceInformat
 import org.finos.legend.engine.protocol.deephaven.metamodel.store.DeephavenStore;
 import org.finos.legend.engine.protocol.deephaven.metamodel.store.Table;
 import org.finos.legend.engine.protocol.deephaven.metamodel.store.Column;
+import org.finos.legend.engine.protocol.deephaven.metamodel.type.BooleanType;
+import org.finos.legend.engine.protocol.deephaven.metamodel.type.ByteType;
+import org.finos.legend.engine.protocol.deephaven.metamodel.type.CharType;
+import org.finos.legend.engine.protocol.deephaven.metamodel.type.CustomType;
+import org.finos.legend.engine.protocol.deephaven.metamodel.type.DoubleType;
+import org.finos.legend.engine.protocol.deephaven.metamodel.type.FloatType;
+import org.finos.legend.engine.protocol.deephaven.metamodel.type.IntType;
+import org.finos.legend.engine.protocol.deephaven.metamodel.type.LongType;
+import org.finos.legend.engine.protocol.deephaven.metamodel.type.ShortType;
+import org.finos.legend.engine.protocol.deephaven.metamodel.type.StringType;
+import org.finos.legend.engine.protocol.deephaven.metamodel.type.TimeType;
+import org.finos.legend.engine.protocol.deephaven.metamodel.type.TypeVisitor;
 import org.finos.legend.pure.generated.*;
 import org.finos.legend.pure.m3.coreinstance.meta.pure.metamodel.type.Class;
 
@@ -45,7 +57,7 @@ public class HelperDeephavenStoreBuilder
     {
         MutableList<Root_meta_external_store_deephaven_metamodel_store_Column> columns = srcTable.columns.stream().map(x -> buildColumn(x, sourceInformation, context)).collect(Collectors.toCollection(Lists.mutable::empty));
         Class<?> classifier = context.pureModel.getClass("meta::external::store::deephaven::metamodel::store::Table");
-        // TODO - ask beyraf - does the pure graph actually need to know about sourceinfo ? probably not... ?
+        // TODO - for things that aren't packageable elements we need to add sourceinfo back in future - sourceinfo is only avail for packageable element equivalents in pure
         return new Root_meta_external_store_deephaven_metamodel_store_Table_Impl(srcTable.name, sourceInformation, classifier)
                 ._name(srcTable.name)
                 ._columns(columns);
@@ -54,10 +66,85 @@ public class HelperDeephavenStoreBuilder
     public static Root_meta_external_store_deephaven_metamodel_store_Column buildColumn(Column srcColumn, org.finos.legend.pure.m4.coreinstance.SourceInformation sourceInformation, CompileContext context)
     {
         Class<?> colClassifier = context.pureModel.getClass("meta::external::store::deephaven::metamodel::store::Column");
-        Class<?> typeClassifier = context.pureModel.getClass("meta::external::store::deephaven::metamodel::store::Type");
-        // TODO - ask beyraf - does the pure graph actually need to know about sourceinfo ? probably not... ?
+        // TODO - for things that aren't packageable elements we need to add sourceinfo back in future - sourceinfo is only avail for packageable element equivalents in pure
         return new Root_meta_external_store_deephaven_metamodel_store_Column_Impl(srcColumn.name, sourceInformation, colClassifier)
                 ._name(srcColumn.name)
-                ._type(new Root_meta_external_store_deephaven_metamodel_type_Type_Impl(srcColumn.type.getClass().getName(), sourceInformation, typeClassifier));
+                ._type(srcColumn.type.accept(new DeephavenTypeVisitor(context)));
+    }
+
+    private static class DeephavenTypeVisitor implements TypeVisitor<Root_meta_external_store_deephaven_metamodel_type_Type>
+    {
+        private final CompileContext context;
+
+        private DeephavenTypeVisitor(CompileContext context)
+        {
+            this.context = context;
+        }
+
+        @Override
+        public Root_meta_external_store_deephaven_metamodel_type_BooleanType visit(BooleanType val)
+        {
+            return new Root_meta_external_store_deephaven_metamodel_type_BooleanType_Impl(val.getClass().getName(), null, this.context.pureModel.getClass("meta::external::store::deephaven::metamodel::type::BooleanType"));
+        }
+
+        @Override
+        public Root_meta_external_store_deephaven_metamodel_type_ByteType visit(ByteType val)
+        {
+            return new Root_meta_external_store_deephaven_metamodel_type_ByteType_Impl(val.getClass().getName(), null, this.context.pureModel.getClass("meta::external::store::deephaven::metamodel::type::ByteType"));
+        }
+
+        @Override
+        public Root_meta_external_store_deephaven_metamodel_type_CharType visit(CharType val)
+        {
+            return new Root_meta_external_store_deephaven_metamodel_type_CharType_Impl(val.getClass().getName(), null, this.context.pureModel.getClass("meta::external::store::deephaven::metamodel::type::CharType"));
+        }
+
+        @Override
+        public Root_meta_external_store_deephaven_metamodel_type_CustomType visit(CustomType val)
+        {
+            return new Root_meta_external_store_deephaven_metamodel_type_CustomType_Impl(val.getClass().getName(), null, this.context.pureModel.getClass("meta::external::store::deephaven::metamodel::type::CustomType"));
+        }
+
+        @Override
+        public Root_meta_external_store_deephaven_metamodel_type_DoubleType visit(DoubleType val)
+        {
+            return new Root_meta_external_store_deephaven_metamodel_type_DoubleType_Impl(val.getClass().getName(), null, this.context.pureModel.getClass("meta::external::store::deephaven::metamodel::type::DoubleType"));
+        }
+
+        @Override
+        public Root_meta_external_store_deephaven_metamodel_type_FloatType visit(FloatType val)
+        {
+            return new Root_meta_external_store_deephaven_metamodel_type_FloatType_Impl(val.getClass().getName(), null, this.context.pureModel.getClass("meta::external::store::deephaven::metamodel::type::FloatType"));
+        }
+
+        @Override
+        public Root_meta_external_store_deephaven_metamodel_type_IntType visit(IntType val)
+        {
+            return new Root_meta_external_store_deephaven_metamodel_type_IntType_Impl(val.getClass().getName(), null, this.context.pureModel.getClass("meta::external::store::deephaven::metamodel::type::IntType"));
+        }
+
+        @Override
+        public Root_meta_external_store_deephaven_metamodel_type_LongType visit(LongType val)
+        {
+            return new Root_meta_external_store_deephaven_metamodel_type_LongType_Impl(val.getClass().getName(), null, this.context.pureModel.getClass("meta::external::store::deephaven::metamodel::type::LongType"));
+        }
+
+        @Override
+        public Root_meta_external_store_deephaven_metamodel_type_ShortType visit(ShortType val)
+        {
+            return new Root_meta_external_store_deephaven_metamodel_type_ShortType_Impl(val.getClass().getName(), null, this.context.pureModel.getClass("meta::external::store::deephaven::metamodel::type::ShortType"));
+        }
+
+        @Override
+        public Root_meta_external_store_deephaven_metamodel_type_StringType visit(StringType val)
+        {
+            return new Root_meta_external_store_deephaven_metamodel_type_StringType_Impl(val.getClass().getName(), null, this.context.pureModel.getClass("meta::external::store::deephaven::metamodel::type::StringType"));
+        }
+
+        @Override
+        public Root_meta_external_store_deephaven_metamodel_type_TimeType visit(TimeType val)
+        {
+            return new Root_meta_external_store_deephaven_metamodel_type_TimeType_Impl(val.getClass().getName(), null, this.context.pureModel.getClass("meta::external::store::deephaven::metamodel::type::TimeType"));
+        }
     }
 }
--- legend-engine-xts-deephaven/legend-engine-xt-deephaven-grammar/src/test/java/org/finos/legend/engine/language/deephaven/compiler/TestDeephavenCompiler.java
+++ legend-engine-xts-deephaven/legend-engine-xt-deephaven-grammar/src/test/java/org/finos/legend/engine/language/deephaven/compiler/TestDeephavenCompiler.java
@@ -14,9 +14,32 @@
 
 package org.finos.legend.engine.language.deephaven.compiler;
 
-import org.eclipse.collections.impl.list.mutable.ListAdapter;
+import org.eclipse.collections.api.RichIterable;
+import org.eclipse.collections.api.factory.Lists;
+import org.eclipse.collections.api.tuple.Pair;
 import org.finos.legend.engine.language.pure.compiler.test.TestCompilationFromGrammar;
+import org.finos.legend.engine.language.pure.compiler.toPureGraph.PureModel;
+import org.finos.legend.engine.protocol.deephaven.metamodel.store.Table;
+import org.finos.legend.engine.protocol.pure.v1.model.context.PureModelContextData;
+import org.finos.legend.pure.generated.Root_meta_external_store_deephaven_metamodel_store_Column;
+import org.finos.legend.pure.generated.Root_meta_external_store_deephaven_metamodel_store_DeephavenStore;
+import org.finos.legend.pure.generated.Root_meta_external_store_deephaven_metamodel_store_Table;
+import org.finos.legend.pure.generated.Root_meta_external_store_deephaven_metamodel_type_BooleanType;
+import org.finos.legend.pure.generated.Root_meta_external_store_deephaven_metamodel_type_BooleanType_Impl;
+import org.finos.legend.pure.generated.Root_meta_external_store_deephaven_metamodel_type_IntType;
+import org.finos.legend.pure.generated.Root_meta_external_store_deephaven_metamodel_type_IntType_Impl;
+import org.finos.legend.pure.generated.Root_meta_external_store_deephaven_metamodel_type_StringType;
+import org.finos.legend.pure.generated.Root_meta_external_store_deephaven_metamodel_type_StringType_Impl;
+import org.finos.legend.pure.generated.Root_meta_external_store_deephaven_metamodel_type_TimeType;
+import org.finos.legend.pure.generated.Root_meta_external_store_deephaven_metamodel_type_TimeType_Impl;
+import org.finos.legend.pure.generated.Root_meta_external_store_deephaven_metamodel_type_Type;
+import org.finos.legend.pure.m3.coreinstance.meta.pure.store.Store;
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class TestDeephavenCompiler extends TestCompilationFromGrammar.TestCompilationFromGrammarTestSuite
 {
@@ -48,5 +71,42 @@ public class TestDeephavenCompiler extends TestCompilationFromGrammar.TestCompil
     protected String getDuplicatedElementTestExpectedErrorMessage()
     {
         return "COMPILATION error at [11:1-18:1]: Duplicated element 'test::Store::foo'";
+    }
+
+    @Test
+    public void testCompileStore() throws Exception
+    {
+        Pair<PureModelContextData, PureModel> result = test("###Deephaven\n" +
+                "Deephaven test::Store::foo\n" +
+                "{\n" +
+                "    Table xyz\n" +
+                "    {\n" +
+                "        prop1: string,\n" +
+                "        prop2: int,\n" +
+                "        prop3: boolean,\n" +
+                "        prop4: datetime\n" +
+                "    }\n" +
+                "}\n"
+        );
+
+        Store store = result.getTwo().getStore("test::Store::foo");
+        Assert.assertTrue(store instanceof Root_meta_external_store_deephaven_metamodel_store_DeephavenStore);
+        RichIterable<? extends Root_meta_external_store_deephaven_metamodel_store_Table> tables = ((Root_meta_external_store_deephaven_metamodel_store_DeephavenStore) store)._tables();
+        Assert.assertEquals(tables.size(), 1);
+        RichIterable<? extends Root_meta_external_store_deephaven_metamodel_store_Column> columns = tables.getAny()._columns();
+        Assert.assertEquals(columns.size(), 4);
+
+        Map<String, Class<? extends Root_meta_external_store_deephaven_metamodel_type_Type>> expectedCols = new HashMap<String, Class<? extends Root_meta_external_store_deephaven_metamodel_type_Type>>();
+        expectedCols.put("prop1", Root_meta_external_store_deephaven_metamodel_type_StringType.class);
+
+        Map<String, Class<? extends Root_meta_external_store_deephaven_metamodel_type_Type>> actualCols = new HashMap<String, Class<? extends Root_meta_external_store_deephaven_metamodel_type_Type>>();
+        columns.forEach(x -> actualCols.put(x._name(), x._type().getClass()));
+
+        Class<Root_meta_external_store_deephaven_metamodel_type_StringType> strCls = Root_meta_external_store_deephaven_metamodel_type_StringType.class;
+
+        Assert.assertTrue(Root_meta_external_store_deephaven_metamodel_type_StringType.class.isAssignableFrom(actualCols.get("prop1")));
+        Assert.assertTrue(Root_meta_external_store_deephaven_metamodel_type_IntType.class.isAssignableFrom(actualCols.get("prop2")));
+        Assert.assertTrue(Root_meta_external_store_deephaven_metamodel_type_BooleanType.class.isAssignableFrom(actualCols.get("prop3")));
+        Assert.assertTrue(Root_meta_external_store_deephaven_metamodel_type_TimeType.class.isAssignableFrom(actualCols.get("prop4")));
     }
 }
