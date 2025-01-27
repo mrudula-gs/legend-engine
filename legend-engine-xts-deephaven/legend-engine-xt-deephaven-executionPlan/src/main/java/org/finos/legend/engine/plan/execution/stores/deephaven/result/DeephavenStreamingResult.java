ca12cabec1b61e6d57939a82d1f9af466733c5f8
mrudula-gs <89876341+mrudula-gs@users.noreply.github.com>
2025-01-27 16:44:57 -0600

    Create DeephavenStreamingResult.java

---
+++ legend-engine-xts-deephaven/legend-engine-xt-deephaven-executionPlan/src/main/java/org/finos/legend/engine/plan/execution/stores/deephaven/result/DeephavenStreamingResult.java
@@ -0,0 +1,77 @@
+// Copyright 2025 Goldman Sachs
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package org.finos.legend.engine.plan.execution.stores.deephaven.result;
+
+import org.finos.legend.engine.plan.execution.result.ExecutionActivity;
+import org.finos.legend.engine.plan.execution.result.Result;
+import org.finos.legend.engine.plan.execution.result.ResultVisitor;
+import org.finos.legend.engine.plan.execution.result.builder.Builder;
+import org.finos.legend.engine.plan.execution.result.builder.stream.StreamBuilder;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+public class DeephavenStreamingResult extends Result //StreamingObjectResult<Map<String, Object>>
+{
+    private final List<Map<String, Object>> rows;
+
+    public DeephavenStreamingResult(List<Map<String, Object>> rows)
+    {
+        this(rows, Collections.emptyList());
+    }
+
+    public DeephavenStreamingResult(List<Map<String, Object>> rows, List<ExecutionActivity> activities)
+    {
+        super("success", activities);
+//        super(iterator);
+//        super(activities);
+        this.rows = rows;
+    }
+
+//    @Override
+//    public void stream(OutputStream outputStream, SerializationFormat format)
+//    {
+//    }
+
+
+//    public List<Map<String, Object>> getRows()
+//    {
+//        return this.rows;
+//    }
+
+    public Builder getResultBuilder()
+    {
+        return new StreamBuilder();
+    }
+
+//    @Override
+//    public Serializer getSerializer(SerializationFormat format)
+//    {
+//        return null;
+//    }
+
+//    @Override
+//    public void close()
+//    {
+//        this.rows.close();
+//    }
+
+    @Override
+    public <V> V accept(ResultVisitor<V> resultVisitor)
+    {
+        throw new UnsupportedOperationException("Streaming DeephavenStreamingResult result is not supported. Please raise a issue with dev team");
+    }
+}
